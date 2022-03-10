1eb2fa0e93694939a8664ec382d777710572cdcb
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-03-10 11:48:43 -0800

    Added SlashInteraction class

--- main.ts
+++ main.ts
@@ -1,1 +1,2 @@
 export { default as Interaction } from "./structures/Interaction";
+export { default as SlashInteraction } from "./structures/SlashInteraction";
---
+++ structures/SlashInteraction.ts
@@ -0,0 +1,50 @@
+import {
+  APIApplicationCommandInteractionDataOption,
+  APIChatInputApplicationCommandInteraction,
+  APIChatInputApplicationCommandInteractionDataResolved,
+  APIInteractionResponseCallbackData,
+  InteractionResponseType,
+} from "discord-api-types/v10";
+import Interaction from "./Interaction";
+
+export default class SlashInteraction extends Interaction {
+  options: APIApplicationCommandInteractionDataOption[];
+  resolved: APIChatInputApplicationCommandInteractionDataResolved;
+  #responded = false;
+  #deferred = false;
+  #deferEdited = true;
+
+  constructor(payload: APIChatInputApplicationCommandInteraction) {
+    super(payload);
+    this.options = payload.data.options ?? [];
+    this.resolved = payload.data.resolved ?? {};
+  }
+
+  /**
+   * Create a deferred message
+   * @param ephemeral Whether the deferred message is ephemeral
+   * @returns The response from Discord
+   */
+  defer(ephemeral: false): Promise<Response> {
+    if (this.#responded) throw new Error("A response has already been sent!");
+    this.#deferred = true;
+    this.#deferEdited = false;
+    return this.respond(
+      InteractionResponseType.DeferredChannelMessageWithSource,
+      ephemeral ? { flags: 64 } : {}
+    );
+  }
+
+  /**
+   * Create a new message. If a response has already been sent, then it will be sent as a follow-up. If `SlashInteraction.defer()` has been called, then it will be sent as an edit.
+   * @param data The message data
+   * @returns The response from Discord
+   */
+  send(data: APIInteractionResponseCallbackData): Promise<Response> {
+    return this.#responded
+      ? this.followup(data)
+      : this.#deferred && !this.#deferEdited
+      ? this.edit(data)
+      : this.respond(InteractionResponseType.ChannelMessageWithSource, data);
+  }
+}
